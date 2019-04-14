f7cd7b62eef6116df52055e043b1e4b2175f4690
JCSweet <sweetjor@gmail.com>
2019-04-14 19:24:04 -0400

    functional and styled

---
+++ src/app/ToDo.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { analyzeAndValidateNgModules } from '@angular/compiler';
+import { FormsModule } from '@angular/forms';
+
+interface Item {
+  task: string;
+  completed: boolean;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './ToDo.component.html',
+  styleUrls: ['./ToDo.component.css']
+})
+
+export class AppComponent {
+  title = 'ToDoListPart1';
+  taskInput: string;
+  completedInput: boolean;
+
+  toDo: Item[] = [
+    { task: 'Groceries', completed: false },
+    { task: 'Meal Prep', completed: false },
+    { task: 'Water Plants', completed: false },
+  ];
+
+  addTask = () => {
+    const newToDo = {
+      task: this.taskInput,
+      completed: this.completedInput,
+    };
+    this.toDo.push(newToDo);
+
+    this.taskInput = null;
+  }
+
+  completeTask = (index) => {
+    this.toDo[index].completed = true;
+  };
+
+  removeTask = (index) => {
+    this.toDo.splice(index, 1)
+  };
+
+}
--- src/app/ToDo.module.ts
+++ src/app/ToDo.module.ts
@@ -1,15 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './ToDo.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule],
+
   providers: [],
   bootstrap: [AppComponent]
 })
